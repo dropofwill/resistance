1f131d20a77602b3bc84925a5447c76498db4c32
Will <wp.neversummer@gmail.com>
2013-09-30 23:09:27 -0400

    init commit

---
+++ ResistWPF/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResistWPF
+{
+    class Game
+    {
+        private int numSpies = 2;
+        private int numPlayers;
+
+        public Game() : this(5) {}
+
+        public Game(int numPlayers)
+        {
+
+        }
+
+    }
+}
